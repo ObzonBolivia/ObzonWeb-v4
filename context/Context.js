38bc5f279d93a828b8a4b89b0fb80fc8f1c62929
ObzonBolivia <123510701+ObzonBolivia@users.noreply.github.com>
2023-09-10 10:08:41 -0400

    Update Context.js
    DC

--- context/Context.js
+++ context/Context.js
@@ -4,36 +4,7 @@ const UserContext = React.createContext()
 
 export function UserProvider({ children }) {
 	// Data de un usuario proveido por FIREBASE AUTHENTICATION
-	const [user, setUser] = useState({
-		"apiKey": "AIzaSyAvAE932To1LPwo3nDu3CvVy-F_DtTnZeg",
-		"authDomain": "obzonbolivia-177c2.firebaseapp.com",
-		"appName": "[DEFAULT]",
-		"currentUser": {
-			"uid": "kVLgYKvifQapEz2C6OY9cwXI3bo1",
-			"email": "tester123@gmail.com",
-			"emailVerified": false,
-			"isAnonymous": false,
-			"providerData": [
-				{
-					"providerId": "password",
-					"uid": "tester123@gmail.com",
-					"displayName": null,
-					"email": "tester123@gmail.com",
-					"phoneNumber": null,
-					"photoURL": null
-				}
-			],
-			"stsTokenManager": {
-				"refreshToken": "AMf-vBxKWYEyVnLvKhvL01WltOZfu4n9gFckfx3y6AJUeq9KOgSNRkYlmMDBiq41kLz3ahGvaxzixtgqvVzHBqwVBaR9RM_mWNg0nEZso6gyMyDnZ3wjH27eMUqE5-5uWV-i-FIUl7uVNN_Aepst9FywT64BbP-U2_G0BJPcug0DY7YdTBGwm8E9a_uYj52zRXxrGTXmZeGebmhA56IxZFelfAE5QgsJqg",
-				"accessToken": "eyJhbGciOiJSUzI1NiIsImtpZCI6IjE5MGFkMTE4YTk0MGFkYzlmMmY1Mzc2YjM1MjkyZmVkZThjMmQwZWUiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJodHRwczovL3NlY3VyZXRva2VuLmdvb2dsZS5jb20vb2J6b25ib2xpdmlhLTE3N2MyIiwiYXVkIjoib2J6b25ib2xpdmlhLTE3N2MyIiwiYXV0aF90aW1lIjoxNjk0MzI0MjA3LCJ1c2VyX2lkIjoia1ZMZ1lLdmlmUWFwRXoyQzZPWTljd1hJM2JvMSIsInN1YiI6ImtWTGdZS3ZpZlFhcEV6MkM2T1k5Y3dYSTNibzEiLCJpYXQiOjE2OTQzMjQyMDcsImV4cCI6MTY5NDMyNzgwNywiZW1haWwiOiJ0ZXN0ZXIxMjNAZ21haWwuY29tIiwiZW1haWxfdmVyaWZpZWQiOmZhbHNlLCJmaXJlYmFzZSI6eyJpZGVudGl0aWVzIjp7ImVtYWlsIjpbInRlc3RlcjEyM0BnbWFpbC5jb20iXX0sInNpZ25faW5fcHJvdmlkZXIiOiJwYXNzd29yZCJ9fQ.Z14T361oRNFUNnmLKOL0XfFWJARkagKzQVbmoPjZUlAi5Fz2O2GjahhCZ04HQs4FjwAO3BnryQwDYXY5BzkSbQ8021bTLOg7sjevBfmGXh11e06u9lLNM6-A5ASejv_T7cA0HpALYzfs8ju0UvZJAujjtLQSapw-KfuqyGZI2l88O9aQ-LoUsvoIIMc6qU4iaBKXmnY-bguHihrlg7gQeQRpEkwDor1aaUXiUKOJLooM8yCctEtpX6Q5ocx4vc3H0B47oSsM04JHgnU7KrjznTLaQmHnFe7dtxqYWCsJQWR_N7RZnp7-XyH1hVRYc5vzNOkiFaZAdFSrnoI0b1tYnw",
-				"expirationTime": 1694327807873
-			},
-			"createdAt": "1684268531053",
-			"lastLoginAt": "1694324207736",
-			"apiKey": "AIzaSyAvAE932To1LPwo3nDu3CvVy-F_DtTnZeg",
-			"appName": "[DEFAULT]"
-		}
-	})
+	const [user, setUser] = useState(undefined)
 	// Data de un usuario proveido por FIREBASE DATABASE
 	const [userDB, setUserDB] = useState('loading')
 	const [success, setSuccess] = useState(null)
